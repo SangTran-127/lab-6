93d7bc563fb454dabc9558afc58999d884d85c56
SangTran-127 <tranquangsang12.7@gmail.com>
2022-11-12 11:10:08 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import 'react-toastify/dist/ReactToastify.css';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { NotificationSliceState, success } from './store/slice/notification.slice';
@@ -9,6 +8,8 @@ import Header from './components/Header';
 import PrivateRoute from './hoc/PrivateRoute';
 import Routes from './Routes';
 function App() {
+
+
   const dispatch = useAppDispatch()
   const notification = useAppSelector(state => state.notification)
   useEffect(() => {
